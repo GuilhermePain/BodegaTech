8774698c19b2ae7f3dee37a7951b93fd589b92ed
GuilhermePain <guizaorocha543@gmail.com>
2024-04-29 15:07:56 -0300

    Modificando função de autenticação do firebase

--- src/components/FormLoginAndRegister/Form.jsx
+++ src/components/FormLoginAndRegister/Form.jsx
@@ -1,29 +1,26 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import styles from '../FormLoginAndRegister/Form.module.css'
 import imageForm from '../../assets/img/undraw_shopping_app_flsj.png'
 import { auth } from '../../services/firebase.js'
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
 
 const Form = ({ titleForm, buttonForm }) => {
 
-  const signIn = (e) => {
+  const inputEmail = useRef()
+  const inputPassword = useRef()
+
+  const signIn = async (e) => {
     e.preventDefault()
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [
-      createUserWithEmailAndPassword,
-      user,
-      loading,
-      error,
-    ] = useCreateUserWithEmailAndPassword(auth);
-  
-    const handleSignIn = () => {
-      createUserWithEmailAndPassword(email, password)
-    }
+    const email = inputEmail.current.value
+    const password = inputPassword.current.value
 
+    try {
+      await auth.createUserWithEmailAndPassword(email, password)
+    } catch(error) {
+      console.log("Erro");
+    }
   }
 
   return (
@@ -31,7 +28,7 @@ const Form = ({ titleForm, buttonForm }) => {
         <aside>
           <img src={imageForm} alt='imagem do formulário' className={styles.imageForm}/>
         </aside>
-        <form className={styles.formContent}>
+        <form onSubmit={signIn} className={styles.formContent}>
           <h1>{titleForm}</h1>
           <hr className={styles.underlineTitle}/>
           <div className={styles.nameArea}>
@@ -43,7 +40,7 @@ const Form = ({ titleForm, buttonForm }) => {
             location.pathname === '/register' && <div>
               <div className={styles.emailArea}>
                 <label className={styles.labelForm}>E-mail</label>
-                <input onChange={(e) => setEmail(e.target.value)}type='email' placeholder='Digite seu e-mail' className={styles.inputForm}/>
+                <input ref={inputEmail} type='email' placeholder='Digite seu e-mail' className={styles.inputForm}/>
               </div>
               <div className={styles.registerInputs}>
                 <div className={styles.phoneArea}>
@@ -59,7 +56,7 @@ const Form = ({ titleForm, buttonForm }) => {
           }
           <div className={styles.passwordArea}>
             <label className={styles.labelForm}>Senha</label>
-            <input type='password' placeholder='Digite sua senha' className={styles.inputForm} onChange={(e) => setPassword(e.target.value)} />
+            <input ref={inputPassword} type='password' placeholder='Digite sua senha' className={styles.inputForm} />
           </div>
           <div className={styles.passwordArea}>
             {
@@ -73,7 +70,7 @@ const Form = ({ titleForm, buttonForm }) => {
               <input type='checkbox'/>
               <label>Mostrar senha</label>
           </div>
-          <button onClick={signIn} type='submit' className={styles.buttonForm}>{buttonForm}</button>
+          <button type='submit' className={styles.buttonForm}>{buttonForm}</button>
           {
           location.pathname === '/login' ?
           <p>Não tem uma conta? <Link to='/register' className={styles.linkForm}>Registrar-se</Link></p> :
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -8,8 +8,10 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
-};
+}
 
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig)
 
-export const auth = getAuth(app);
+const auth = app.auth()
+
+export { auth }
