9bc00f43fe737760e470a29bfc8be69013b110ad
GuilhermePain <guizaorocha543@gmail.com>
2024-07-16 09:24:09 -0300

    Adição do caminho para a página que irá separar o cadastro de supermercado de cliente.

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,17 @@ import './main.css';
 import LandingPage from "./pages/LandingPage.jsx";
 import Login from "./pages/Login";
 import Register from "./pages/Register.jsx"
+import UserType from "./pages/UserType.jsx";
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingPage/>,
+  },
+  {
+    path: "tipodeconta",
+    element: <UserType />
   },
   {
     path: "/login",
